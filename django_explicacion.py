27089f588ec3d064524e1a491371ee0e4f4373be
Martin Avelen <martinboyb@gmail.com>
2023-12-14 22:52:33 -0300

    add

---
+++ django_explicacion.py
@@ -0,0 +1,33 @@
+#pip install django: 
+"instala el paquete django para usar los templates y comandos del mismo."
+
+#python -m django startproject Proyectodjango: Ejecutar en terminal
+"""Crea la carpeta (Proyectodjango)
+Con las configuraciones de django y el modulo manage.py para poder trabajar."""
+
+"""Manage nos ayuda a interactuar con nuestro proyecto con algunos comandos muy simples. Ademas
+Lo que genera este comando de primeras es un archivo manage, que es el encargado de casi todo lo que queramos solicitarle al proyecto, 
+y una carpeta con los archivos iniciales necesarios para iniciar con configuraciones básicas."""
+
+#python manage.py migrate: 
+"""Migra todo a una server de django y nos brinda una url"""
+
+#python manage.py runserver: 
+"""Nos crea y corre el servidor en internet"""
+
+#Crtl + C: 
+"""para salir del server y usar la terminal"""
+
+
+#--------------------------------------------------------------------------------
+
+### Entornos virtuales ###
+
+"""Un entorno virtual en Python es una herramienta 
+que te permite crear un espacio aislado donde puedes instalar 
+y manejar las dependencias (bibliotecas y versiones de Python) 
+específicas para un proyecto en particular, sin afectar al sistema global de Python."""
+
+#pip list : muestra las biblotecas instaladas en el entorno virtual o GLOBAL.
+
+#Como crear un entorno virtual? (entorno aislado del global)
