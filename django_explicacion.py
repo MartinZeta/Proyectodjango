d4aff52462ee965800ba62dddec6b2b7d7b0fe97
Martin Avelen <martinboyb@gmail.com>
2023-12-16 18:52:04 -0300

    Add git merge dev

--- django_explicacion.py
+++ django_explicacion.py
@@ -36,3 +36,5 @@ específicas para un proyecto en particular, sin afectar al sistema global de Py
 ### me quede en el min 47 de la clase.
 
 #git log --oneline: nos sirve para saber a donde esta apuntando (HEAD-CABEZA), si es a la rama principal, a la de desarrollo y al origin que seria github
+
+#git merge dev: Une las ramas, de desarrollo con la principal para guardar todos los cambios en la principal, antes que nada hay que moverse a la rama princi.
