2adbf31eb18f08fcdbd3de5a6a2891603bbe97c0
Martin Avelen <martinboyb@gmail.com>
2023-12-16 20:22:53 -0300

    Comandos django

--- django_explicacion.py
+++ django_explicacion.py
@@ -1,25 +1,3 @@
-#pip install django: 
-"instala el paquete django para usar los templates y comandos del mismo."
-
-#python -m django startproject Proyectodjango: Ejecutar en terminal
-"""Crea la carpeta (Proyectodjango)
-Con las configuraciones de django y el modulo manage.py para poder trabajar."""
-
-"""Manage nos ayuda a interactuar con nuestro proyecto con algunos comandos muy simples. Ademas
-Lo que genera este comando de primeras es un archivo manage, que es el encargado de casi todo lo que queramos solicitarle al proyecto, 
-y una carpeta con los archivos iniciales necesarios para iniciar con configuraciones básicas."""
-
-#python manage.py migrate: 
-"""Migra todo a una server de django y nos brinda una url"""
-
-#python manage.py runserver: 
-"""Nos crea y corre el servidor en internet"""
-
-#Crtl + C: 
-"""para salir del server y usar la terminal"""
-
-
-#--------------------------------------------------------------------------------
 """
 #- git branch: no dice cuantas ramas tenemos creadas.
 
@@ -44,12 +22,64 @@ específicas para un proyecto en particular, sin afectar al sistema global de Py
 
 #Como crear un entorno virtual? (entorno aislado del global)
 
-"""
-- pip list : muestra las biblotecas instaladas en el entorno virtual o GLOBAL.
 
-- python -m venv .venv : Crea el entorno virtual. 
+#- pip list : 
+"""
+muestra las biblotecas instaladas en el entorno virtual o GLOBAL.
+"""
+
+#- python -m venv .venv : 
+"""
+Crea el entorno virtual. 
 Antes de hacer el entorno virtual, se debe crear un modulo llamado dentro de el escribir .venv (.gitignore:
 le dice a git que no suba este paquete o archivo a internet que lo ignore.)
 """
 
 #Como activar el entorno virtual?
+
+#.\.venv\Scripts\activate : 
+"""
+con este comando activamos el entorno virtual.
+"""
+
+#--------------------------------------------------------------------------------
+
+
+#### Instalar Django en entorno virtual? ####
+
+""" 
+Esto permite que en este entorno solo quede una version que Django o otros paquetes que instalemos
+y si salen actualizaciones que no proboque un error. ¡¡¡¡¡Tambien tiene que estar activo el entorno virtual!!!!, esto para que no lo instale en otro entorno.
+"""
+
+## Comandos
+
+#pip install django:
+"""instala el paquete django para usar los templates y comandos del mismo."""
+
+"""Creamos la carpeta Proyectodjango"""
+
+#-cd Proyectodjango: 
+"""
+Luego hay que moverse a la carpeta que creamos, en este caso, Proyectodjango
+"""
+
+#django-admin --version:
+"""Nos dice la version de django"""
+
+#python -m django startproject Proyectodjango: Ejecutar en terminal
+"""Crea la carpeta (Proyectodjango)
+Con las configuraciones de django y el modulo manage.py para poder trabajar."""
+
+"""Manage nos ayuda a interactuar con nuestro proyecto con algunos comandos muy simples. Ademas
+Lo que genera este comando de primeras es un archivo manage, que es el encargado de casi todo lo que queramos solicitarle al proyecto, 
+y una carpeta con los archivos iniciales necesarios para iniciar con configuraciones básicas."""
+
+#python manage.py migrate: 
+"""Migra todo a una server de django y nos brinda una url"""
+
+#python manage.py runserver: 
+"""Nos crea y corre el servidor en internet"""
+
+#Crtl + C: 
+"""para salir del server y usar la terminal"""
